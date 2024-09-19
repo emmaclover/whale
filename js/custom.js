f63cc8d97d2aca0f304c0da74b95f2871361dfea
emmaclover <devsignjay7@gmail.com>
2024-09-19 10:00:12 +0900

    09-19 term commit

--- js/custom.js
+++ js/custom.js
@@ -5,10 +5,21 @@ $(function(){
   // GNB Include Load
   $('.gnb-main').load('/include/gnb.html')
 
-  $('.select-All').c
 
 
+   // Signin Field
+  $('.all-term').on('change', function() {
+    // all-ter 모두 체크 선택 and 해제
+    $('.chk-term').prop('checked', $(this).prop('checked'));
+  });
 
-
+  // 개별 체크 시 전체 체크 해제
+  $('.chk-term').on('change', function() {
+    if ($('.chk-term:checked').length === $('.chk-term').length) {
+      $('.all-term').prop('checked', true);
+    } else {
+      $('.all-term').prop('checked', false);
+    }
+  });
 
 })
