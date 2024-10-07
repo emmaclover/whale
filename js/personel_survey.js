8b399c46e407ed1e62fb18e1e7e647d1e05283e1
isyouhoney <isu9848@gmail.com>
2024-10-07 09:55:05 +0900

    fix:html id fixed

--- js/personel_survey.js
+++ js/personel_survey.js
@@ -5,16 +5,14 @@ window.addEventListener("flutterInAppWebViewPlatformReady", function(event) {
 
 $(document).ready(function() {
 
-  // 버튼 클릭 시 입력값을 가져오는 로직
   $('.enter-btn').on('click', function() {
-    // 입력값을 클릭 시점에 가져옴
     const name = document.getElementById('userName').value;
-    const gender = document.querySelector('input[name="gender"]:checked')?.value;
+    const gender = document.querySelector('gender')?.value;
     const birthday = document.getElementById('birthDate').value;
     const job = document.getElementById('userJob').value;
     const sleepTime = document.getElementById('sleepTime').value;
     const wakeTime = document.getElementById('wakeTime').value;
-    const sedentaryTime = document.getElementById('time-select').value;
+    const sedentaryTime = document.getElementById('sitTime').value;
 
     // 가져온 데이터를 객체로 변환
     const surveyData = {
@@ -27,11 +25,9 @@ $(document).ready(function() {
       sedentaryTime: sedentaryTime,
     };
 
-    // JSON 형식으로 변환
     const surveyDataJSON = JSON.stringify(surveyData);
 
-//      console.log("전송 데이터:", surveyDataJSON);
-    // InAppWebView에 데이터 전송
+      console.log("전송 데이터:", surveyDataJSON);
     if (window.flutter_inappwebview) {
       window.flutter_inappwebview.callHandler('personel-survey', surveyDataJSON)
         .catch(function(error) {
