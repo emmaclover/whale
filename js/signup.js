04bf53174ecd26c1b6f50710f3e74604de68ca71
morris <dltkdah777@gmail.com>
2024-10-15 10:00:25 +0900

    fix : webview와 연동되지 않는 문제 수정

--- js/signup.js
+++ js/signup.js
@@ -1,37 +1,30 @@
-$(function(){
-  $(document).ready(function() {
-    $('.age-year-input-field').on('input', function() {
-      if ($(this).val().length > 4) {
-        $(this).val($(this).val().slice(0, 4));  // 4자리 이상 입력 시 잘라냄
-      }
+// flutterInAppWebViewPlatformReady 이벤트 리스너
+window.addEventListener("flutterInAppWebViewPlatformReady", function(event) {
+  console.log("WebView is ready for communication.");
+});
+
+$(document).ready(function() {
+  $('.signup-btn').on('click', function() {
+    const password = document.getElementById('password').value;
+    const emailId = document.getElementById('email-id').value;
+    const emailAddress = document.getElementById('email-address').value;
+
+    const signupData = JSON.stringify({
+      email: emailId + '@' + emailAddress,
+      password: password
     });
-  });
 
-  $(document).ready(function() {
-    $('.signup-btn').on('click', function() {
-      if (window.flutter_inappwebview) {
-        window.flutter_inappwebview.callHandler('signup', '회원가입 버튼이 클릭되었습니다.')
-          .catch(function(error) {
-            console.error("Error sending data to Flutter: ", error);
-          });
-      } else {
-        console.error("Flutter InAppWebView is not ready.");
-      }
-    });
-  });
-
-   // Signin Field
-   $('.all-term').on('change', function() {
-    // all-ter 모두 체크 선택 and 해제
-    $('.chk-term').prop('checked', $(this).prop('checked'));
-  });
-
-  // 개별 체크 시 전체 체크 해제
-  $('.chk-term').on('change', function() {
-    if ($('.chk-term:checked').length === $('.chk-term').length) {
-      $('.all-term').prop('checked', true);
+    if (window.flutter_inappwebview) {
+        window.flutter_inappwebview.callHandler('signup', signupData)
+        .catch(function(error) {
+          console.error("Error sending data to Flutter: ", error);
+        });
     } else {
-      $('.all-term').prop('checked', false);
+      console.error("Flutter InAppWebView is not ready.");
     }
   });
+});
+
+$('.header-include').load('../include/header.html', function() {
+
 })
